fcf5fb60b4a0b20879d83b67daa00051207c60b1
SizeLee <nothingforname@gmail.com>
2018-06-06 10:21:53 +0800

    finish spider work

--- spider.py
+++ spider.py
@@ -5,76 +5,6 @@ import time
 import json
 import re
 
-def get_news_pool(root, start, end):
-    news_pool = []
-    for i in range(start,end,-1):
-        page_url = ''
-        if i != start:
-            page_url = root +'_%d.shtml'%(i)
-        else:
-            page_url = root + '.shtml'
-        try:
-            response = urllib.request.urlopen(page_url)
-        except Exception as e:
-            print("-----%s: %s-----"%(type(e), page_url))
-            continue
-        html = response.read()
-        soup = BeautifulSoup(html,"lxml") # http://www.crummy.com/software/BeautifulSoup/bs4/doc.zh/
-        td = soup.find('td', class_ = "newsblue1")
-        a = td.find_all('a')
-        span = td.find_all('span')
-        for i in range(len(a)):
-            date_time = span[i].string
-            url = a[i].get('href')
-            title = a[i].string
-            news_info = ['2016-'+date_time[1:3]+'-'+date_time[4:-1]+':00',url,title]
-            news_pool.append(news_info)
-    return(news_pool)
-
-def crawl_news(url, levels, link_file_dic):
-    if levels == 0:
-        return
-    connect = urllib.request.urlopen(url)
-    content = connect.read()
-    time.sleep(2)
-    soup = BeautifulSoup(content, 'lxml')
-    print(soup.prettify)
-    # todo add url to dic
-    link_file_dic[url] = 0
-    pass #################################
-
-    if levels != 1:
-        # todo find next level's links
-        a = soup.find_all('a')
-        nextlevel = []
-        for each in a:
-            s = each.get('class')
-            if s == ['link-home']:
-                l = each.get('href')
-                if l.startswith('//w'):
-                    l = 'http:' + l
-                elif l.startswith('/a'):
-                    l = 'http://www.sohu.com' + l
-                nextlevel.append(l)
-
-        print(nextlevel)
-        for each in nextlevel:
-            if each in link_file_dic:
-                continue
-            crawl_news(each, levels-1, link_file_dic)
-                
-
-
-    return
-
-
-# rooturl = ['https://en.wikipedia.org/wiki/Machine_learning']
-# # rooturl = ['http://m.sohu.com/ch/8?_f=m-article_ch']
-# # rooturl = ['http://m.sohu.com/a/234071193_162758?_f=m-channel_8_focus_3']
-# # testurl = 'http://www.sohu.com/a/233949131_267106?_f=index_chan08news_1'
-# links = []
-# link_file_dic = {}
-
 class wiki_spider:
     def __init__(self, rooturl):
         self.rooturl = rooturl
@@ -104,7 +34,7 @@ class wiki_spider:
             complete_url = self.url_head + url
         else:
             return
-        print(complete_url)
+        # print(complete_url)
         connection = urllib.request.urlopen(complete_url)
         content = connection.read()
         time.sleep(1)
@@ -116,7 +46,7 @@ class wiki_spider:
         # print(content.find('div'))
         main_body = content.find('div', id='bodyContent').find('div', id='mw-content-text').find('div')
         paragraphs = main_body.find_all('p')
-        text = ''
+        text = title + '\n'
         for eachgraph in paragraphs:
             parastr = ''
             for string in eachgraph.strings:
@@ -149,7 +79,6 @@ class wiki_spider:
         return
             
             
-
 
 
 if __name__ == '__main__':
